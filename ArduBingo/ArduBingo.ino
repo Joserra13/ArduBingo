c359941553d9731a9223fd9e6325085f99270ef0
Joserra <Joserra_013@hotmail.com>
2024-11-29 19:25:34 +0100

    Feat: Add interrupts to trigger events

--- ArduBingo/ArduBingo.ino
+++ ArduBingo/ArduBingo.ino
@@ -10,12 +10,19 @@
 #include <LiquidCrystal.h>
 
 //Defines------------------------------------
-#define N 90
+#define N 91
 #define BTN1 2
 #define BTN2 3
 
 //Initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+
+//Global Variables
+int list[N];
+int empties = N - 1;
+int ball;
+int getNumberFlag = false;
+int seeListFlag = false;
 
 void setup() {
   //Pin config
@@ -30,94 +37,119 @@ void setup() {
   lcd.setCursor(5, 1);
   lcd.print("BINGO!");
 
-  randomSeed(analogRead(0));  // Get the seed from an analog input -> Random each time
+  // Interrupt on falling edge
+  attachInterrupt(digitalPinToInterrupt(BTN1), setNumberFlag, FALLING);
+  attachInterrupt(digitalPinToInterrupt(BTN2), setListFlag, FALLING);
 
+  // Get the seed from an analog input -> Random each time
+  randomSeed(analogRead(0));
+
+  //Initialize the Serial port
   Serial.begin(9600);
   delay(1000);
+
+  //item 0 from list -- unused
+  list[0] = 999;
+}
+
+void setNumberFlag(){
+  getNumberFlag = true;
+}
+
+void setListFlag(){
+  seeListFlag = true;
+}
+
+void getNumber() {
+  /*
+    * Generate a random number between 1 and 90.
+    * If the number has already been drawn (already appears in the list), generate a new number.
+    */
+
+  do {
+    ball = random(1, N);  // Generates a number from 1 to 90
+  } while (list[ball] != 0);
+
+  Serial.print(ball);
+  Serial.println();
+
+  empties--;
+  list[ball] = ball;
+
+  //Now, let's print the value in the lcd
+  lcd.clear();
+  // set the cursor to column 0, line 0
+  lcd.setCursor(0, 0);
+  lcd.print("Numero: ");
+  lcd.print(list[ball]);
+  lcd.setCursor(0, 1);
+  lcd.print("1:Next    2:List");
+}
+
+void seeList() {
+  for (int i = 1; i < N; i++) {
+
+    //Print to LCD
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("list[");
+    lcd.print(i);
+    lcd.print("] = ");
+    lcd.print(list[i]);
+
+    //Print to Serial Monitor
+    Serial.print("list[");
+    Serial.print(i);
+    Serial.print("] = ");
+    Serial.print(list[i]);
+    Serial.println();
+
+    if (list[i] != 0) {
+      delay(700);
+    } else {
+      delay(250);
+    }
+
+    if (digitalRead(BTN1) == LOW) {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Numero: ");
+      lcd.print(list[ball]);
+      lcd.setCursor(0, 1);
+      lcd.print("1:Next    2:List");
+      break;
+    }
+  }
 }
 
 void loop() {
-  int i = 0;
-  int list[N];
-  int empties = N;
-  int ball;
 
-  lcd.clear();
-
-  for (i = 0; i <= N; i++) {
+  for (int i = 1; i < N; i++) {
     list[i] = 0;
   }
 
   while (empties > 0) {
-    /*
-    * Generate a random number between 0 and 89.
-    * If the number has already been drawn (already appears in the list), generate a new number.
-    */
 
-    do {
-      ball = random(N);  // Generates a number from 0 to 89
-    } while (list[ball] != 0);
+    if(getNumberFlag){
 
-    empties--;
+      getNumber();
+      getNumberFlag = false;
+    } else if(seeListFlag){
 
-    /*
-    * Since the random numbers are between 0 and 89 and we want them between 1 and N, we store ball + 1.
-    * If we directly generate a number between 1 and N and use it to index the array,
-    * the problem arises when the number N is generated, and you try to index list[N],
-    * as this is a memory location you don't have access to. 
-    * Your array has N positions: from 0 to 89.
-    */
-
-    list[ball] = ball + 1;
-
-    lcd.clear();
-    // set the cursor to column 0, line 0
-    lcd.setCursor(0, 0);
-    lcd.print("Numero: ");
-    lcd.print(list[ball]);
-    lcd.setCursor(0, 1);
-    lcd.print("1:Next    2:List");
-
-    here:
-    delay(300);
-
-    while (digitalRead(BTN1) == HIGH) {
-      if (digitalRead(BTN2) == LOW) {
-        // for(i = 1; i < 91; i++) // This can cause problems because you don't have access to list[N]
-        for (i = 0; i < N; i++) {
-          lcd.clear();
-          lcd.setCursor(0, 0);
-          lcd.print("list[");
-          lcd.print(i);
-          lcd.print("] = ");
-          lcd.print(list[i]);
-          Serial.print("list[");
-          Serial.print(i);
-          Serial.print("] = ");
-          Serial.print(list[i]);
-          Serial.println();
-
-          if (list[i] != 0) {
-            delay(700);
-          } else {
-            delay(200);
-          }
-
-          if (digitalRead(BTN2) == LOW) {
-            goto here;
-          }
-        }
-      }
+      seeList();
+      seeListFlag = false;
     }
   }
 
   lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("      GAME       ");
   lcd.setCursor(0, 1);
-  lcd.print("   GAME   OVER   ");
+  lcd.print("      OVER       ");
 
   while (digitalRead(BTN1) == HIGH) {
     lcd.blink();
   }
 
-  delay(2000);
+  delay(5000);
 }
